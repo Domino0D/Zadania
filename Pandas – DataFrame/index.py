4fe52089f660832796e26bd3818aa410da6a16b1
Domino0D <147524381+Domino0D@users.noreply.github.com>
2024-05-20 23:53:14 +0200

    Update index.py

--- Pandas – DataFrame/index.py
+++ Pandas – DataFrame/index.py
@@ -15,7 +15,7 @@ sales_df['Month'] = sales_df['Date'].dt.month
 
 sales_df = sales_df.merge(stores_df[['Store', 'Type', 'Size']], on='Store', how='left')
 
-monthly_sales_counts = sales_df.groupby(['Year', 'Month', 'Store']).size().groupby(['Year', 'Month']).size()
+monthly_sales_counts = sales_df.groupby(['Year', 'Month', 'Store']).size().groupby(['Year', 'Month']).count().reset_index(name='Liczba sklepów')
 
 average_weekly_sales_by_year = sales_df.groupby('Year')['Weekly_Sales'].mean()
 
@@ -37,21 +37,15 @@ average_sales_by_year_and_type = sales_df.groupby(['Year', 'Type'])['Weekly_Sale
 average_sales_per_size_by_year_and_type = sales_df.groupby(['Year', 'Type'])['Sales_per_Size'].mean()
 
 # Wyniki
-print("Liczba sklepów danego typu:\n", store_types_counts)
-print(store_types_counts.to_string(header=False, name=False))
-print("\nStatystyki dla rozmiaru sklepów:\n", size_statistics)
-print(size_statistics.to_string(header=False, name=False))
+print("Liczba sklepów danego typu (A, B, C):\n", store_types_counts.to_string(index=False, header=False))
+print("\nStatystyki dla rozmiaru sklepów:\n", size_statistics.to_string(index=False, header=False))
 print("\nStatystyki dla rozmiaru sklepów w podziale na typy:\n", size_statistics_by_type)
 print("\nLiczba sklepów prowadzących sprzedaż w każdym miesiącu każdego roku:\n", monthly_sales_counts)
-print("\nŚredni poziom tygodniowej sprzedaży dla każdego roku:\n", average_weekly_sales_by_year)
-print("\nŚrednia tygodniowa sprzedaż zależna od występowania święta:\n", average_sales_holiday)
-print(average_sales_holiday.to_string(header=False, name=False))
-print("\nŚrednia tygodniowa sprzedaż charakteryzująca się sezonowością roczną:\n", monthly_average_sales)
-print(monthly_average_sales.to_string(header=False))
+print("\nŚredni poziom tygodniowej sprzedaży dla każdego roku:\n", average_weekly_sales_by_year.to_string(index=False))
+print("\nŚrednia tygodniowa sprzedaż zależna od występowania święta:\n", average_sales_holiday.to_string(index=False, header=False))
+print("\nŚrednia tygodniowa sprzedaż charakteryzująca się sezonowością roczną:\n", monthly_average_sales.to_string())
 print("\nSklepy z najniższą i najwyższą średnią tygodniową sprzedażą w 2012 roku:\n", lowest_sales_store_2012, highest_sales_store_2012)
 print("\nSklepy z najniższą i najwyższą średnią tygodniową sprzedażą na jednostkę powierzchni w 2012 roku:\n", lowest_sales_per_size_store_2012, highest_sales_per_size_store_2012)
-print("\nŚredni poziom tygodniowej sprzedaży oraz sprzedaży na jednostkę powierzchni dla każdego roku i typu sklepu:\n", average_sales_by_year_and_type)
-print(average_sales_by_year_and_type.to_string(header=False, name=False))
-print("\n", average_sales_per_size_by_year_and_type)
-print(average_sales_per_size_by_year_and_type.to_string(header=False, name=False))
+print("\nŚredni poziom tygodniowej sprzedaży oraz sprzedaży na jednostkę powierzchni dla każdego roku i typu sklepu:\n", average_sales_by_year_and_type.to_string())
+print("\n", average_sales_per_size_by_year_and_type.to_string())
 
